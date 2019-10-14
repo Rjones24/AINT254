83f200a4f401cdbae207771bd6932a20911f0507
unknown <ryan.jvjvjones@ntlworld.com>
2019-10-14 17:08:06 +0100

    fixes to ball spawning and movable head so minor camera moving

--- CourseWork/lizard_game/movement/toungue and movements/Assets/Scripts/Camera_look.cs
+++ CourseWork/lizard_game/movement/toungue and movements/Assets/Scripts/Camera_look.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class Camera_look : MonoBehaviour
+
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float LookVertical = Input.GetAxisRaw("Controller Y");
-        transform.Rotate(-LookVertical/2, 0f , 0f, Space.Self);
+        float LookVertical = Input.GetAxisRaw("Controller Y") ;
+        transform.Rotate(-LookVertical /6, 0f, 0f, Space.Self);
     }
 }
